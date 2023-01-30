6290dcaa66fe0624e02ff3a7a8cbf70ae92c23ac
Diana <dianam0@rambler.ru>
2023-01-30 19:41:55 +0300

    added maven.compiler.source and maven.compiler.target properties

--- yookassa-payment-service/src/main/java/ru/itis/tdportal/paymentservice/configs/AppConfig.java
+++ yookassa-payment-service/src/main/java/ru/itis/tdportal/paymentservice/configs/AppConfig.java
@@ -1,5 +1,11 @@
 package ru.itis.tdportal.paymentservice.configs;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11Nio2Protocol;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import ru.itis.tdportal.common.clients.configs.OpenFeignConfig;
@@ -9,4 +15,21 @@ import ru.itis.tdportal.liquibase.configs.LiquibaseConfig;
 @Configuration
 @Import({PortalCoreConfig.class, LiquibaseConfig.class, OpenFeignConfig.class})
 public class AppConfig {
+
+    @Value("${server.http.port}")
+    private int httpPort;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector(Http11Nio2Protocol.class.getName());
+        connector.setPort(httpPort);
+        return connector;
+    }
+
 }
--- yookassa-payment-service/src/main/java/ru/itis/tdportal/paymentservice/dtos/CreatedPaymentDto.java
+++ yookassa-payment-service/src/main/java/ru/itis/tdportal/paymentservice/dtos/CreatedPaymentDto.java
@@ -1,5 +1,6 @@
 package ru.itis.tdportal.paymentservice.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -7,12 +8,16 @@ import lombok.NoArgsConstructor;
 import ru.itis.tdportal.common.models.dtos.PaymentDto;
 
 import java.util.Map;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class CreatedPaymentDto extends PaymentDto {
+
+    @JsonProperty("id")
+    private UUID yooId;
 
     private Map<String, String> confirmation;
 
