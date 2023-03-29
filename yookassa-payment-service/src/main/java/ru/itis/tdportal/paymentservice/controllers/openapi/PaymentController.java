3c51d30301cafd8e177ba6f21f36fd7bb2881784
Diana <dianam0@rambler.ru>
2023-03-30 00:03:50 +0300

    feature/3DPORTAL-19: fixed audit

--- yookassa-payment-service/src/main/java/ru/itis/tdportal/paymentservice/controllers/openapi/PaymentController.java
+++ yookassa-payment-service/src/main/java/ru/itis/tdportal/paymentservice/controllers/openapi/PaymentController.java
@@ -1,7 +1,15 @@
 package ru.itis.tdportal.paymentservice.controllers.openapi;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.itis.tdportal.paymentservice.services.PaymentService;
 
 import java.util.Map;
@@ -15,6 +23,14 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PutMapping("/{idempotenceKey}")
+    @ApiResponses(value =
+    @ApiResponse(content = {
+            @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(example = "{\"confirmationUrl\": \"string\", \"type\": \"redirect\"}")
+            )
+    })
+    )
     public Map<String, String> movePaymentToStatusPending(
             @PathVariable UUID idempotenceKey) {
         return paymentService.movePaymentToStatusPending(idempotenceKey);
