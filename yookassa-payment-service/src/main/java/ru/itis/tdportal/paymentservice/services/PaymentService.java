c505456651e315e043c8d5928fd44c291f39c6ee
Diana <dianam0@rambler.ru>
2023-04-18 21:39:15 +0300

    3DPORTAL-22: fixed exception catching

--- yookassa-payment-service/src/main/java/ru/itis/tdportal/paymentservice/services/PaymentService.java
+++ yookassa-payment-service/src/main/java/ru/itis/tdportal/paymentservice/services/PaymentService.java
@@ -11,16 +11,17 @@ import ru.itis.tdportal.common.models.dtos.PaymentDto;
 import ru.itis.tdportal.common.models.entities.Money;
 import ru.itis.tdportal.common.models.enums.PaymentStatus;
 import ru.itis.tdportal.paymentservice.dtos.CreatedPaymentDto;
+import ru.itis.tdportal.paymentservice.dtos.EventObjectDto;
 import ru.itis.tdportal.paymentservice.dtos.PayoutDto;
 import ru.itis.tdportal.paymentservice.dtos.YookassaNotificationDto;
 import ru.itis.tdportal.paymentservice.models.entities.Payment;
 import ru.itis.tdportal.paymentservice.models.enums.ConfirmationType;
-import ru.itis.tdportal.paymentservice.models.enums.PayoutStatus;
 import ru.itis.tdportal.paymentservice.models.exceptions.PaymentNotFoundException;
 import ru.itis.tdportal.paymentservice.models.mappers.PaymentMapper;
 import ru.itis.tdportal.paymentservice.repositories.PaymentRepository;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
@@ -35,6 +36,7 @@ public class PaymentService {
     private final PaymentMapper paymentMapper;
     private final PaymentRepository repository;
     private final YookassaService yookassaService;
+
     private final PayoutService payoutService;
 
     @Transactional
@@ -84,29 +86,41 @@ public class PaymentService {
 
     @Transactional
     public void movePaymentToStatusByEvent(YookassaNotificationDto dto) {
-        // TODO: проверка на NPE
-        Payment payment = getByYooIdOrThrow(dto.getObject().getId());
-        PaymentStatus status = dto.getEvent().getObjectStatus();
-        payment.setStatus(status);
-
-        mainServiceClient.updatePaymentStatusNotification(payment.getIdempotenceKey(), status);
-
-        if (PaymentStatus.SUCCEEDED.equals(status)) {
-            PayoutDto payoutDto = new PayoutDto();
-            payoutDto.setPayoutToken(payment.getReceiver().getPayoutToken());
-
-            Money paymentAmount = payment.getAmount();
-            // TODO: должна быть нормальная формула для расчета выплаты по заказу
-            payoutDto.setAmount(new MoneyDto(paymentAmount.getValue(), paymentAmount.getCurrency()) );
-
-            payoutDto.setDescription(String.format("Выплата по заказу %s", payment.getIdempotenceKey()));
-
-            payoutService.createPayout(new PayoutDto(), payment.getIdempotenceKey());
+        EventObjectDto object = dto.getObject();
+        if (Objects.isNull(object) || Objects.isNull(object.getId())) {
+            log.error(
+                    String.format("Received invalid event %s", object)
+            );
+            return;
         }
 
-        log.info(String.format(
-                "Payment %s successfully moved to status %s",
-                payment.getId(), status)
-        );
+        PaymentStatus status = dto.getEvent().getObjectStatus();
+        log.info(String.format("Payment moving %s to status %s started", object.getId(), status));
+        try {
+            Payment payment = getByYooIdOrThrow(dto.getObject().getId());
+            payment.setStatus(status);
+
+            mainServiceClient.updatePaymentStatusNotification(payment.getIdempotenceKey(), status);
+
+            if (PaymentStatus.SUCCEEDED.equals(status)) {
+                PayoutDto payoutDto = new PayoutDto();
+                payoutDto.setPayoutToken(payment.getReceiver().getPayoutToken());
+
+                Money paymentAmount = payment.getAmount();
+
+                payoutDto.setAmount(new MoneyDto(paymentAmount.getValue(), paymentAmount.getCurrency()));
+                payoutDto.setDescription(String.format("Выплата по заказу %s", payment.getIdempotenceKey()));
+
+                payoutService.createPayout(payoutDto, payment.getIdempotenceKey());
+            }
+
+            log.info(
+                    String.format("Payment %s successfully moved to status %s", payment.getId(), status)
+            );
+        } catch (Exception e) {
+            log.error(String.format(
+                    "Payment moving %s to status %s failed causing: %s", dto.getEvent()
+            ));
+        }
     }
 }
